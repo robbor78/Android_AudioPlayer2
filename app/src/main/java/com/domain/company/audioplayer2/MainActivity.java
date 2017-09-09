810ab9abf1c9c94f25cb57208fe4aa194bdd6574
robbbor78 <robbbor78@gmail.com>
2017-09-09 19:00:32 +0200

    before bind

--- app/src/main/java/com/domain/company/audioplayer2/MainActivity.java
+++ app/src/main/java/com/domain/company/audioplayer2/MainActivity.java
@@ -1,27 +1,91 @@
 package com.domain.company.audioplayer2;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
+    private String filePath;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate called "+savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startService();
+        if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
+            filePath = getIntent().getData().getPath();
+
+            TextView t = (TextView) findViewById(R.id.tvInfo);
+            t.setText(filePath);
+
+            Log.d(TAG, "onCreate: " + filePath);
+
+            startService(filePath);
+
+        } else {
+            Log.d(TAG, "onCreate no intent");
+
+            //filePath = savedInstanceState.getString("FilePath");
+            TextView t = (TextView) findViewById(R.id.tvInfo);
+            t.setText(filePath);
+
+        }
 
     }
 
-    private void startService() {
-        Intent intent = new Intent(this, PlayerService.class);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("FilePath",filePath);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d(TAG, "received new intent...");
+        filePath = intent.getStringExtra("FilePath");
+        TextView t = (TextView) findViewById(R.id.tvInfo);
+        t.setText(filePath);
+
+
+
+    }
+
+    private void startService(String filePath) {
+        intent = new Intent(this, PlayerService.class);
+
+        try {
+            stopService(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        intent.putExtra("FilePath", filePath);
         startService(intent);
         Log.d(TAG, "starting service...");
     }
--- app/src/main/java/com/domain/company/audioplayer2/PlayerService.java
+++ app/src/main/java/com/domain/company/audioplayer2/PlayerService.java
@@ -7,10 +7,16 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
+
 public class PlayerService extends IntentService {
 
     private static final String TAG = "PlayerService";
     private static final int ONGOING_NOTIFICATION_ID = 1;
+
+    private boolean stop = false;
 
     public PlayerService() {
         super("PlayerService");
@@ -19,21 +25,38 @@ public class PlayerService extends IntentService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "player starting", Toast.LENGTH_SHORT).show();
-        Log.d(TAG,"player starting...");
-        return super.onStartCommand(intent,flags,startId);
+        Log.d(TAG, "player starting...");
+        return super.onStartCommand(intent, flags, startId);
     }
+
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+        Log.d(TAG,"onDestroy");
+        super.onDestroy();
+        stop = true;
+    }
+
 
     @Override
     protected void onHandleIntent(Intent intent) {
 
+        stop = false;
+        String filePath = intent.getStringExtra("FilePath");
+
         Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);//FLAG_ACTIVITY_NEW_TASK);
+        notificationIntent.putExtra("FilePath",filePath);
         PendingIntent pendingIntent =
-                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+                PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
 
         Notification notification =
                 new Notification.Builder(this)
-                        .setContentTitle(getText(R.string.notification_title))
-                        .setContentText(getText(R.string.notification_message))
+                        .setContentTitle("AP2 Playing")
+                        .setContentText(filePath)
                         .setSmallIcon(R.drawable.notification_icon)
                         .setContentIntent(pendingIntent)
                         .setTicker(getText(R.string.ticker_text))
@@ -42,8 +65,8 @@ public class PlayerService extends IntentService {
         startForeground(ONGOING_NOTIFICATION_ID, notification);
 
         try {
-            while (true) {
-                Log.d(TAG,"handling intent...");
+            while (!stop) {
+                Log.d(TAG, "handling intent... "+filePath);
                 Thread.sleep(10000);
             }
         } catch (InterruptedException e) {
